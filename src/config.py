f3cd5a4cd67b923570b72122f2b0aab388068e20
woffee <iwoffee@gmail.com>
2025-08-12 21:13:58 -0400

    update folder path

--- src/config.py
+++ src/config.py
@@ -4,5 +4,5 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 LOG_DIR = os.path.join('logs')
 UPLOAD_FOLDER = os.path.join('uploads')
 OUTPUT_FOLDER = os.path.join('outputs')
-TMP_FOLDER = '/var/tmp'
+TMP_FOLDER = os.path.join('outputs')
 ALLOWED_EXTENSIONS = {'csv', 'txt', 'xlsx'}
